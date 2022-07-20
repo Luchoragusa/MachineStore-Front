f34315ca448ef51ee17dcae4b7f0459727eb6341
JuaniPucheta <80986033+JuaniPucheta@users.noreply.github.com>
2022-07-20 19:55:11 -0300

    cambios

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from 'src/app/interfaces/game';
 
 @Component({
   selector: 'app-card',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() game!: Game;
+  
   constructor() { }
 
   ngOnInit(): void {
