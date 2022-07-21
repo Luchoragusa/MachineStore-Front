6a22b78c6ce547bb9ad64cad9ffe93dcaafe664e
JuaniPucheta <80986033+JuaniPucheta@users.noreply.github.com>
2022-07-20 23:47:53 -0300

    login to home

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,14 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent{
-show = false;
-onClicksignUp(event: any){
-  console.log('Pulsaste el boton para registrarte', event)
-  this.show = true;
-}
+  show = false;
   
-onClicksignIn(event: any){
-  console.log('Pulsaste el boton para registrarte', event)
-  this.show = false;
-}
+  constructor(private _router: Router) { }
+
+  onClicksignUp(event: any){
+    console.log('Pulsaste el boton para registrarte', event)
+    
+    this.show = true;
+  }
+    
+  onClicksignIn(event: any){
+    console.log('Pulsaste el boton para registrarte', event)
+    this._router.navigate(['/home']);
+    this.show = false;
+  }
 }
