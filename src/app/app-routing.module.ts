a74666d8731fc2fc5041eaf602878aa3fc9de8c6
JuaniPucheta <80986033+JuaniPucheta@users.noreply.github.com>
2022-07-20 22:42:21 -0300

    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,28 +5,26 @@ import { RouterModule, Routes, Route } from '@angular/router';
 import { CardContainerComponent } from './componentes/card-container/card-container.component';
 import { CardDetailsComponent } from './componentes/card-details/card-details.component';
 
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: CardContainerComponent,
+        children: [
+            {
+                path: ':cardId',
+                component: CardDetailsComponent
+            }
+        ]
+    }
+];
 
-
-// const routes: Routes = [
-//     {
-//         path: '',
-//         redirectTo: 'cards',
-//         pathMatch: 'full'
-//     },
-//     {
-//         path: 'cards',
-//         component: CardContainerComponent,
-//         children: [
-//             {
-//             path: ':cardId',
-//             component: CardDetailsComponent
-//             }
-//         ]
-//     }
-// ];
-
-// @NgModule({
-//     imports: [RouterModule.forRoot(routes)],
-//     exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/componentes/card-details/card-details.component.ts
+++ src/app/componentes/card-details/card-details.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Game } from '../../interfaces/game';
+import { GamesService } from '../../services/games.service';
+
 @Component({
   selector: 'app-card-details',
   templateUrl: './card-details.component.html',
   styleUrls: ['./card-details.component.css']
 })
-export class CardDetailsComponent implements OnInit {
+export class CardDetailsComponent {
 
-  constructor() { }
+  game!: Game;
+  subscription!: Subscription;
+  id = '';
 
-  ngOnInit(): void {
-  }
+  constructor(
+    private _activatedRoute: ActivatedRoute
+    // private _gamesService: GamesService
+  ) {
+    if (this._activatedRoute.snapshot.params['cardId']) {
+      this._activatedRoute.params.subscribe((params: Params) => {
+        if (params['cardId']) {
+          this.id = params['cardId'];
+        }
+      });
+    }
+      
+    }
 }
