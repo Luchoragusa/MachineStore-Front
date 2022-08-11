6006022a3c6970e6cbf7bea8bd837e563446ff18
Luchoragusa <luchito.rc4@gmail.com>
2022-08-10 23:39:34 -0300

    la tengo muy grande

--- src/app/componentes/game-founded/game-founded.component.ts
+++ src/app/componentes/game-founded/game-founded.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Game, GamesResponse } from 'src/app/interfaces/game';
 import { GamesService } from 'src/app/services/games.service';
@@ -10,45 +10,23 @@ import { GamesService } from 'src/app/services/games.service';
 })
 export class GameFoundedComponent implements OnInit {
 
-  gamesResponse!: GamesResponse;
-  games!: Game[];
+  @Input() gamesResponse!: GamesResponse;
 
-  constructor(private route: ActivatedRoute, private _games: GamesService) {
-    // this._games.getGames().subscribe({
-    //     next: (gamesResponse: GamesResponse) => {
-    //       this.gamesResponse = gamesResponse;
-    //       this.games = this.gamesResponse.games;
-    //       console.log("Traigo todos los juegos para el filtrado");
-    //       console.log(this.games);
-    //     },
-    // });
+  constructor(private route: ActivatedRoute) {
   }
 
   games_search: Game[] = [];
 
    ngOnInit(): void {
 
-    // _games: GamesService
-    console.log("Traigo todos los juegos para el filtrado");
-    this._games.getGames().subscribe({
-      next: (gamesResponse: GamesResponse) => {
-        this.gamesResponse = gamesResponse;
-        this.games = this.gamesResponse.games;
-        console.log("Traigo todos los juegos para el filtrado");
-        console.log(this.games);
-      }
-    }
-    
-    );
+    console.log(this.gamesResponse.games);
 
-    // console.log("En el onInit");
-    // console.log(this.games);
-    // this.route.params.subscribe(params => {
-    //   if (params['searchTerm']) {
-    //     this.games_search = this.gamesResponse.games.filter(game => game.name.toLowerCase().includes(params['searchTerm'].toLowerCase()));
-    //   } else {
-    //     this.gamesResponse.games = this.gamesResponse.games;
-    //   }
-    // });
+    this.route.params.subscribe(params => {
+      if (params['searchTerm']) {
+        this.games_search = this.gamesResponse.games.filter(game => game.name.toLowerCase().includes(params['searchTerm'].toLowerCase()));
+      } else {
+        this.gamesResponse.games = this.gamesResponse.games;
+      }
+    });
    }
 }
