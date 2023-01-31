b4b442285d498fef4c1f6ece3a0c411c3e50a67d
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2023-01-31 12:10:08 -0300

    starting delete-game

--- src/app/modules/admin/games-list/delete-game/delete-game.component.ts
+++ src/app/modules/admin/games-list/delete-game/delete-game.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Game } from 'src/app/modules/games/interface/game';
+import { ServicioService } from 'src/app/modules/services/servicio.service';
+declare let alertify: any;
 
 @Component({
   selector: 'app-delete-game',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteGameComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject (MAT_DIALOG_DATA) public data: Game,
+    private sS:ServicioService ) { }
+
+  juego: Game = this.data;
 
   ngOnInit(): void {
+  }
+
+  deleteGame() {
+    alertify.confirm('¿Estás seguro de que quieres eliminar este juego?', () => {
+      this.sS.deleteTypeOf(this.data.id, 'game').subscribe((response: any) => {
+        alertify.success('Juego eliminado correctamente');
+      });
+    }, function () {})
   }
 
 }
