d75f083f178a18ba856afac24bd2cfd6c6736a73
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2023-01-31 11:28:15 -0300

    edit-game y admin

--- src/app/modules/admin/games-list/edit-game/edit-game.component.ts
+++ src/app/modules/admin/games-list/edit-game/edit-game.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Game } from 'src/app/modules/games/interface/game';
+import { ServicioService } from 'src/app/modules/services/servicio.service';
+declare let alertify: any;
 
 @Component({
   selector: 'app-edit-game',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditGameComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Game,
+    private sS: ServicioService
+  ) { }
+
+  juego: Game = this.data;
+  editdata: any;
 
   ngOnInit(): void {
+    this.loadEditData(this.data.id);
+  }
+
+  form = new FormGroup({
+    name: new FormControl(''),
+    nameCategory: new FormControl(''),
+    nameDeveloper: new FormControl(''),
+    isAvailable: new FormControl(''),
+  })
+
+  loadEditData(id: any) {
+    this.sS.getGame(id).subscribe((response: any) => {
+      this.editdata = response;
+      this.form.patchValue({
+        name: this.editdata.name,
+        nameCategory: this.editdata.Category.name,
+        nameDeveloper: this.editdata.Developer.name,
+        isAvailable: this.editdata.isAvailable,
+      });
+    });
+  }
+
+  guardarJuego() {
+    if(this.form.valid) {
+      this.sS.updateTypeOf(this.data.id, this.data, 'game').subscribe((response: any) => {
+        alertify.success('Juego actualizado correctamente');
+      });
+    }
   }
 
 }
