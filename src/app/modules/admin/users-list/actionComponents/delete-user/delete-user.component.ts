f3d2d14b8d24616b3660724d79baf0ac54b023c1
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2023-01-31 10:41:18 -0300

    edit-user works but idRole no

--- src/app/modules/admin/users-list/actionComponents/delete-user/delete-user.component.ts
+++ src/app/modules/admin/users-list/actionComponents/delete-user/delete-user.component.ts
@@ -1,8 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+// import { FormBuilder } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { User } from 'src/app/modules/users/interface/user';
 import { UsersService } from 'src/app/modules/users/services/users.service';
+declare let alertify: any;
 
 @Component({
   selector: 'app-delete-user',
@@ -12,14 +13,19 @@ import { UsersService } from 'src/app/modules/users/services/users.service';
 export class DeleteUserComponent implements OnInit {
 
   constructor(
-    @Inject (MAT_DIALOG_DATA) public data: User, 
+    @Inject (MAT_DIALOG_DATA) public data: User,
     private uS:UsersService ) { }
+
+  usuario: User = this.data;
 
   ngOnInit(): void {
   }
 
-  deleteUser(){
-    this.uS.deleteUser(this.data.id).subscribe((response: any) => {
-    });
+  deleteUser() {
+    alertify.confirm('¿Estás seguro de que quieres eliminar este usuario?', () => {
+      this.uS.deleteUser(this.data.id).subscribe((response: any) => {
+        alertify.success('Usuario eliminado correctamente');
+      });
+    }, function () {})
   }
 }
--- src/app/modules/admin/users-list/actionComponents/edit-user/edit-user.component.ts
+++ src/app/modules/admin/users-list/actionComponents/edit-user/edit-user.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { User } from 'src/app/modules/users/interface/user';
+import { UsersService } from 'src/app/modules/users/services/users.service';
+declare let alertify: any;
 
 @Component({
   selector: 'app-edit-user',
@@ -8,21 +12,42 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: User,
+    private uS: UsersService
+  ) { }
+
+  usuario: User = this.data;
+  editdata: any;
 
   ngOnInit(): void {
+    this.loadEditData(this.data.id);
   }
 
   form = new FormGroup({
-    nombre: new FormControl('', Validators.required),
+    name: new FormControl(''),
     email: new FormControl('', Validators.email),
-    role: new FormControl(''),
-    fechaCreacion: new FormControl(''),
+    // idRole: new FormControl(''),
+    createdAt: new FormControl(''),
   })
+
+  loadEditData(id:any) {
+    this.uS.getUser(id).subscribe((response: any) => {
+      this.editdata = response;
+      this.form.setValue({
+        name: this.editdata.name,
+        email: this.editdata.email,
+        // idRole: this.editdata.idRole,
+        createdAt: this.editdata.createdAt
+      })
+    })
+  }
 
   guardarUsuario() {
     if(this.form.valid) {
-      console.log(this.form.value);
+      this.uS.updateUser(this.data.id, this.data).subscribe((response: any) => {
+        alertify.success('Usuario actualizado correctamente');
+      });
     }
   }
 
