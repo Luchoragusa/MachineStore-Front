e413d1f56d8caa8afa9dda734d6aa670f4f73371
Luchoragusa <luchito.rc4@gmail.com>
2023-02-14 17:27:39 -0300

    createGame

--- src/app/modules/admin/games-list/create-game/create-game.component.ts
+++ src/app/modules/admin/games-list/create-game/create-game.component.ts
@@ -1,11 +1,12 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Category } from 'src/app/modules/categories/interface/category';
 import { Developer } from 'src/app/modules/developers/interface/developer';
 import { Game } from 'src/app/modules/games/interface/game';
 import { AlertifyService } from 'src/app/modules/services/alertify.service';
 import { ServicioService } from 'src/app/modules/services/servicio.service';
+import { AlertDialogComponent } from 'src/app/modules/shared';
 
 @Component({
   selector: 'app-create-game',
@@ -20,6 +21,7 @@ export class CreateGameComponent implements OnInit {
     private sS: ServicioService,
     private alertify: AlertifyService,
     private _formBuilder: FormBuilder,
+    public dialog: MatDialog
   ) { }
 
   categories: Category[] = this.data.categories;
@@ -34,6 +36,7 @@ export class CreateGameComponent implements OnInit {
       category:     [''],
       developer:    [''],
       description : [''],
+			image :       [''],
       trailer:      [''],
       valoration:   [''],
       isAvailable:  [''],
@@ -41,23 +44,39 @@ export class CreateGameComponent implements OnInit {
     });
   }
 
+	onFileSelected(event: any) {
+    this.gameForm.controls['image'].setValue(event.target.files[0]);
+  }
+
 	addGame() {
     if (this.gameForm.valid) {
-      // const gameEdited = {
-      //   id: this.game.id,
-      //   name: this.gameForm.value.name,
-      //   idCategory: this.category,
-      //   idDeveloper: this.developer,
-      //   isAvailable: this.gameForm.value.isAvailable,
-      //   description: this.gameForm.value.description,
-      //   trailer: this.gameForm.value.trailer,
-      //   valoration: this.gameForm.value.valoration,
-      // }
+      const formData = new FormData();
+      formData.append('name', this.gameForm.value.name || '');
+      formData.append('category', this.gameForm.value.category || '');
+      formData.append('developer', this.gameForm.value.developer || '');
+      formData.append('description', this.gameForm.value.description || '');
+      formData.append('trailer', this.gameForm.value.trailer || '');
+      formData.append('valoration', this.gameForm.value.valoration || '');
+      formData.append('isAvailable', this.gameForm.value.isAvailable || '');
+      formData.append('date', this.gameForm.value.date || '');
+      formData.append('image', this.gameForm.value.image || '');
 
-      // this.sS.updateTypeOf(gameEdited.id, gameEdited, 'game').subscribe((response: any) => {
-      //   this.alertify.success('Juego actualizado correctamente');
-      // });
+      console.log(formData);
 
+      this.sS.createTypeOf(formData, "game").subscribe({
+        next: (response: any) => {
+          console.log(response);
+        },
+        error: (error: any) => {
+          console.log(error);
+          this.dialog.open(AlertDialogComponent, {
+            data: {
+              title: 'Error',
+              message: error.error.message
+            }
+          });
+        }
+      });
     } else {
       this.alertify.error('No se han podido actualizar los datos');
     }
